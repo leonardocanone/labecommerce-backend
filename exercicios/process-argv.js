909e1d0e35e2d63fc15dc8b2c410f40bc305beb4
leonardocanone <leonardocanone@gmail.com>
2023-11-23 17:12:36 -0300

    Projeto labecommerce backend - etapa Node e package.json

---
+++ exercicios/process-argv.js
@@ -0,0 +1,9 @@
+
+// console.log("Testando Exercício 2");
+
+console.log(process.argv[2], process.argv[3]);
+
+function parOuImpar (parImpar, numero) {
+    console.log(process.argv[2], process.argv[3])
+}
+parOuImpar()
---
+++ index.js
@@ -0,0 +1,1 @@
+console.log("Teste Exercício 1");
